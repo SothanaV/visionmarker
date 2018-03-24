281b339e2b0a8d167c27bbbe32c23d4a4d72a927
taratep <taratep.sira@gmail.com>
2018-03-25 00:06:12 +0700

    setup script

--- beta/setup.sh
+++ beta/setup.sh
@@ -3,5 +3,5 @@
 ##source activate django3
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser
+python manage.py shell < init_db.py
 #python manage.py shell_plus --notebook
