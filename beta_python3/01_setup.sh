ff02f06931ed80b8035ff6eaad640433ab380cba
SothanaV <nai6320@gmail.com>
2019-01-08 14:10:18 +0700

    update migrate --run-syncdb django2

--- beta_python3/01_setup.sh
+++ beta_python3/01_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 python manage.py makemigrations
-python manage.py migrate
+python manage.py migrate --run-syncdb
 python manage.py createsuperuser
 python manage.py shell_plus --notebook
